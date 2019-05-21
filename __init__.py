1021c2659cfb058c36e06376245699e56143f80f
PeterNtp <50370873+PeterNtp@users.noreply.github.com>
2019-05-21 12:25:43 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from PedroH import *
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='PedroH',
+      version='1.0',
+      description='Do cool stuff',
+      author='Peter',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['PedroH'],
+     )
